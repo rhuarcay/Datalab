334819d4f7c8f753f01926b324a043594fea69f7
dluck <d.luck@tu-bs.de>
2020-11-11 16:34:00 +0100

    Featureextraktion
    Einige Features händisch extrahiert. Mal schauen ob das was bringt

--- Unit_2/Dennis/Unit2_Ex1.py
+++ Unit_2/Dennis/Unit2_Ex1.py
@@ -9,13 +9,24 @@ FILE = '../Data/Data_Ex1.zip'
 import zipfile as zipp
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-
+#Gibt die Namen der Dateien und die Label dazu zurück
 def extrakt_Names(file):
     zf = zipp.ZipFile(file)
     name = zf.namelist()
     name = name[:-1]
-    return name
+    labels = extrakt_Labels(name)
+    return name, labels
 
+#Erwartet eine Liste an Dateinamen, und extrahiert daraus das Label.
+#Nur für das Training relevant
+def extrakt_Labels(nameList):
+    labels_i=[]
+    for name in nameList:
+        tok = name.split('.')
+        labels_i.append(int(tok[1]))
+    return labels_i
+
+#Extrahiert die Namen aller Dateien aus den Docx-Dateien
 def extrakt_Tree(nameList):
     features_tree=[]
     for names in nameList:
@@ -34,6 +45,9 @@ def extrakt_Tree(nameList):
             continue
     return features_tree
 
+#Verarbeitet die Liste von Listen von Dateien zu einer Liste an Strings
+#Die Dateinamen werden dabei einfach nacheinander in einen String gespeichert
+#Und in eine Liste geworfen
 def listlist_to_stringlist(l):
     stringlist = []
     for entry in l:
@@ -43,6 +57,8 @@ def listlist_to_stringlist(l):
         stringlist.append(string)
     return stringlist
 
+#Verarbeitet die Liste von Listen von Dateien zu einer Liste an überhaupt existierenden files
+#Praktisch für das Extrahieren von features. Maybe.
 def get_filenames(l):
     string_list = []
     for entry in l:
@@ -52,6 +68,10 @@ def get_filenames(l):
     string_list.sort()
     return string_list
 
+#Erwartet als Input eine Liste an Liste von Dateien aus den Docx-Dateien.
+#Dabei wird gezählt, welche Dateinamen wie oft in den Dokumenten vorkommen
+#Rückgabe ist ein Dictionray, wobei die keys die Filenamen und die Values
+#die Anzahl der Files sind
 def count_filenames(featurelist):
     file_dict = {}
     filelist = get_filenames(featurelist)
@@ -65,14 +85,59 @@ def count_filenames(featurelist):
             
     return file_dict
 
+#Erwartet als Input eine Liste an Liste von Dateien aus den Docx-Dateien.
+#Rückgabe ist eine Liste, ob die Datei  'vbaProject.bin' in der Liste ist
+#oder nicht. (1 oder 0)
+def feature_vbaProject(filelist):
+    featurelist = []
+    for l in filelist:
+        if 'vbaProject.bin' in l:
+            featurelist.append(1)
+        else:
+            featurelist.append(0)
+    return featurelist
+
+#Erwartet als Input eine Liste an Listen von Dateien aus den Docx-Dateien.
+#Rückgabe ist eine Liste, ob Mediendateien vorhanden sind. (1 oder 0)
+#Bei count = True wird stattdessen die Anzahl an vorhandenen Bildern zurückgegeben.
+def feature_pictures(filelist, count = False):
+    featurelist = []
+    pic_ends = ['png', 'gif', 'jpg', 'jpeg', 'wdp', 'emf', 'wmf', 'JPG', 'PNG', 'EMF', 'jpe']
+    for l in filelist:
+        pic_exists = 0
+        for name in l:
+            tok = name.split('.')
+            if tok[-1] in pic_ends:
+                if not count:
+                    pic_exists = 1
+                else:
+                    pic_exists = pic_exists + 1
+        featurelist.append(pic_exists)
+    return featurelist
+
+def feature_badzipfile(filelist):
+    featurelist = []
+    for l in filelist:
+        if 'BadZipfile' in l:
+            featurelist.append(1)
+        else:
+            featurelist.append(0)
+    return featurelist
+
 #-------------------------------------------------
 namelist = []
 text = []
 
-namelist = extrakt_Names(FILE)
+namelist, labellist = extrakt_Names(FILE)
 text = extrakt_Tree(namelist)
+
+vbaList = feature_vbaProject(text)
+picList = feature_pictures(text)
+picCount = feature_pictures(text, count=True)
+badzip = feature_badzipfile(text)
+#textlist = get_filenames(text)
+#file_dict = count_filenames(text)
+
 #textlist = listlist_to_stringlist(text)
-textlist = get_filenames(text)
-file_dict = count_filenames(text)
 #cv = CountVectorizer(encoding='utf-32')
 #cv.fit(textlist)
