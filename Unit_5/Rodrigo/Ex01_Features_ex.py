71a235cfdab0c0976a30c0f934dfffa512c90f24
rhuarcay <38560284+rhuarcay@users.noreply.github.com>
2021-01-18 13:12:00 +0100

    updates

--- Unit_5/Rodrigo/Ex01_Features_ex.py
+++ Unit_5/Rodrigo/Ex01_Features_ex.py
@@ -3,6 +3,7 @@ import pyshark
 import os
 import pickle
 import numpy as np
+import re
 
 
 MY_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -10,6 +11,7 @@ SERVER = '192.168.178.30'
 FILE = "nids-train.pcap"
 LOCALDIR = "../Data/nids-train.pcap"
 TESTOUTLIER = "../Data/outlier.pcapng"
+__ERRORLIST = []
 
 '''
 Features FTP Payload:
@@ -17,12 +19,11 @@ Features FTP Payload:
 (2)- Number of Words DONE
 (3)- Index: (1)/(2) DONE
 (4)- Entropy: TODO Methoden zur berechnung von Entropy
-(5)- Nonprint Zeichen
-(6)- Punkte Zeichen
-(7) max lenght Wort
+(5)- Nonprint Zeichen DONE
+(6)- Punkte Zeichen DONE
 '''
 
-def get_tcp_payload(pkt):
+def get_tcp_payload(pkt, tcpstream):
     '''
 
     :param pkt:
@@ -31,13 +32,19 @@ def get_tcp_payload(pkt):
     tcp_payload = 0
     bytes_payload = 0
     k_string = ""
-    words_in_paylaod = 0
+    words_in_payload = 0
+    words = 0
     feature_3 = 0
+    nonprint = 0
+    delimeter = "/","_", " ", "-", ",", ".", "\\"
+    regexPattern = '|'.join(map(re.escape, delimeter))
+    
     try:
         # Hex Darstellung der Payload
         tcp_payload = pkt.tcp.payload
     except Exception as pl:
         print("Problem bei der TCP Payload " + str(pl))
+        set_error_list(str(pl), tcpstream)
 
     try:
         #Count bytes in Payload
@@ -45,6 +52,7 @@ def get_tcp_payload(pkt):
         #print("Anzahl an Bytes: " + str(bytes_payload))
     except Exception as e0:
         print("Problem bei der Payload_Bytes " + str(e0))
+        set_error_list(str(e0), tcpstream)
 
     try:
         #String für die Payload
@@ -53,13 +61,15 @@ def get_tcp_payload(pkt):
         #print("Text : " + k_string)
     except Exception as e1:
         print("Problem bei der Payload_String " + str(e1))
+        set_error_list(str(e1), tcpstream)
 
     try:
         #Anzahl an Words bestimmen
-        words_in_payload = len(k_string.split())
+        words_in_payload = len(re.split(regexPattern, k_string))
         #print("Anzahl an Wörter: " + str(words_in_paylaod))
     except Exception as e2:
         print("Problem bei der TCP Payload " + str(e2))
+        set_error_list(str(e2), tcpstream)
 
     try:
         #Index Bytes Payload / Words
@@ -70,13 +80,33 @@ def get_tcp_payload(pkt):
         print("Problem bei der Payload_index " + str(e3))
         print("Payload_byte: " + str(bytes_payload))
         print("String: " + str(k_string))
+        set_error_list(str(e3), tcpstream)
+        
+    try:
+        #Anzahl an Punkte im Payload
+        punkts_in_payload = str(tcp_payload).count("2e")
+    except Exception as e6:
+        print("Problem bei der Anzahl an Nullwerte " + str(e6))
+        set_error_list(str(e6), tcpstream)
+        
+    #Now loop für alle characters in 
+    for char in str(tcp_payload).split(":"):
+        try:
+            bytes.fromhex(char).decode('ASCII')
+        except:
+            nonprint += 1
+            
+    return tcp_payload, bytes_payload, k_string, words_in_payload, feature_3, nonprint, punkts_in_payload
 
-    return tcp_payload, bytes_payload, k_string, words_in_payload, feature_3
-
+def set_error_list(error, tcpstream):
+    err = str(tcpstream) + " -- " + str(error)
+    __ERRORLIST.append(err)
+    
+    
 
 def features_ex(file):
 
-    features = np.empty((139000, 6), dtype=object)
+    features = np.empty((138022, 8), dtype=object)
 
     cap = pyshark.FileCapture(file)
     i = 0
@@ -86,22 +116,23 @@ def features_ex(file):
     for pkt in cap:
 
         k_string = ""
-        if "FTP" in str(pkt.layers) and str(pkt.ip.src) != "192.168.178.30":
+        if "FTP" in str(pkt.layers) and str(pkt.ip.src) != "192.168.178.30" and str(pkt.tcp.payload).strip() != "0d:0a":
             #Stream Index für den Packet
             stream_index = int(pkt.tcp.stream)
 
 
             #Payload extrahieren
-            hex_payload, feature1, s_Payload, feature2, feature3 = get_tcp_payload(pkt)
+            hex_payload, feature1, s_Payload, feature2, feature3, feature5, feature6 = get_tcp_payload(pkt, stream_index)
             #print("TCP_S: " + str(stream_index) + " Packet: " + str(i) + "  Bytes: " + str(feature1) + " Words: " + str(feature2) + " 1/2Index: " + str(feature3))
 
             features[i, 0] = stream_index # TCP Stream Index Nr.
-            features[i, 1] = hex_payload
-            features[i, 2] = s_Payload # String Payload
-            features[i, 3] = feature1 # Bytes FTP
-            features[i, 4] = feature2 # Nr. Words in Payload
-            features[i, 5] = feature3 # KPI(Bytes/Index)
-
+            features[i, 1] = str(hex_payload).strip()
+            features[i, 2] = s_Payload.strip() # String Payload
+            features[i, 3] = int(feature1) # Bytes FTP
+            features[i, 4] = int(feature2) # Nr. Words in Payload
+            features[i, 5] = int(feature3) # KPI(Bytes/Index)
+            features[i, 6] = int(feature5) # Non Printable Char
+            features[i, 7] = int(feature6) # Punkte im Text
 
 
             i += 1
@@ -125,8 +156,8 @@ def main():
     print("Root: " + str(root))
     print("Features are being extracted")
     features = features_ex(root)
-    write_Out_file("Features.txt", features)
-    #write_Out_file("TestData_Labels.txt", labels)
+    write_Out_file("Features.txt", features) # Features List
+    write_Out_file("Errors.txt", __ERRORLIST) # Error List
 
     print("Features Extraction is done")
 
---
+++ Unit_5/Rodrigo/Ex01_Model_Training.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 13:23:54 2021
+
+@author: Rodrigo
+"""
+import os
+import numpy as np
+import zipfile as zipp
+import pickle
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.svm import OneClassSVM
+from sklearn.ensemble import IsolationForest
+from sklearn import preprocessing
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+
+def write_In_file(in_file):
+    """Writes a List to an outfile in the current directory"""
+    name = in_file
+    with open(in_file, "rb") as in_file:    
+        in_list = pickle.load(in_file)
+    
+    return in_list
+
+def write_to_output(outlier_tcpstream):
+    f = open("outliers_model.txt", "wt")
+    
+    for tcp_stream in outlier_tcpstream:
+        f.write("%s" % tcp_stream + '\n')
+    
+    f.close()
+
+def normalise_features(features):
+        
+    #features = Normalizer().fit_transform(features)
+    features = StandardScaler().fit_transform(features) 
+    return features
+
+def manuel_outlier_detec(features):
+    outliers=[]
+    
+    for counter, pkt in enumerate(features):
+        if pkt[3] > 150:
+            outliers.append(pkt)
+    
+    return outliers
+
+def outlier_detec():
+        
+    #fit model
+    clf = LocalOutlierFactor()
+    #clf = OneClassSVM(kernel='rbf', gamma=0.1, verbose=True, nu=0.1)
+    return clf
+
+def outlier_entfernung(klassification, tcp_stream_index):
+    pkt_set = {0}
+    for counter, pkt in enumerate(klassification):
+        if pkt == -1:
+            pkt_set.add(counter)
+    outlier_tcpstream = {0}
+    for pkt in pkt_set:
+        outlier_tcpstream.add(tcp_stream_index[pkt])
+    return outlier_tcpstream
+
+'''
+def main():
+    #For Trainings Daten
+    total_features = write_In_file("Features.txt") #Features in List
+    tcp_stream_index = total_features[:137952,0] #Index of Stream TCP + Src.ip etc
+    features = total_features[:137952,3:8]
+    
+    features = normalise_features(features)
+    clf = outlier_detec()
+    klassification = clf.fit_predict(features)
+    
+    #-----
+    
+    
+    
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
+'''
+# NUR FÜR TESTING SONST IN MAIN
+#For Trainings Daten
+print("Importing Files")
+total_features = write_In_file("Features.txt") #Features in List
+tcp_stream_index = total_features[:137952,0] #Index of Stream TCP + Src.ip etc
+#features = total_features[:137952,5:6] #Bytes
+features = total_features[:137952,6:7] #Index
+#features = np.concatenate((features,features2),axis = 1)
+#manuel_out = manuel_outlier_detec(total_features[:137952,0:7])
+
+#print("Normierung")
+#features = normalise_features(features)
+clf = outlier_detec()
+print("Outlier detection")
+klassification = clf.fit_predict(features)
+print("Erkennung von TCP_Stream")
+outlier_tcpstream = outlier_entfernung(klassification, tcp_stream_index)
+print("Writing a file out")
+write_to_output(sorted(outlier_tcpstream))
